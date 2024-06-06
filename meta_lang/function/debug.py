a52132d8cc12cad07d282ce7b15542d13aa22ed5
Nathan Clark <nc8774@gmail.com>
2024-06-06 08:53:46 -0400

    adding debug display all

--- meta_lang/function/debug.py
+++ meta_lang/function/debug.py
@@ -1,9 +1,11 @@
 from typing import Dict
 
+from termcolor import colored
+
 from .base import GLOBALS, Program, compile_all
 
 def display_all(programs: Dict[str, Program] = GLOBALS.programs, root_dir: str = './datapacks/testing/data'):
     compiled = compile_all(programs, root_dir, debug=True)
     for file_path, serialized_file in compiled.items():
-        print(file_path)
+        print(colored(file_path, attrs=['bold', 'underline']))
         print(serialized_file)
