a831bdfc48bc96877cf58939d91082fdb12598d2
Nathan Clark <nc8774@gmail.com>
2024-08-04 10:29:08 -0400

    improved handling of code blocks

--- src/metaautolang/parse/malpy/malparse.py
+++ src/metaautolang/parse/malpy/malparse.py
@@ -1,14 +1,27 @@
 import ast
+from typing import TextIO
+import re
 
 from pegen.tokenizer import tokenize, Tokenizer
 from parse import GeneratedParser
 # python -m pegen mcpy.gram
 
+CODE_BLOCK_PLACEHOLDER = '__CODE_BLOCK__'
+
 class Parser:
     def __init__(self):
         pass
 
-    def parse(self, file):
+    def preprocess(self, code: str):
+        pattern = re.compile(r'`([^`]*)`')
+        def replacement(match: re.Match[str]) -> str:
+            content = match.group(1)
+            escaped_content = repr(content)
+            return f'{CODE_BLOCK_PLACEHOLDER}({escaped_content})'
+        
+        return pattern.sub(replacement, code)
+
+    def parse(self, file: TextIO):
         tokengen = tokenize.generate_tokens(file.readline)
         tokenizer = Tokenizer(tokengen, verbose=False)
         parser = GeneratedParser(tokenizer, verbose=False)
